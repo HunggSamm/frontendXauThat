7373197ffa890131c973b5c807495da46f14df02
HunggSamm <143309756+HunggSamm@users.noreply.github.com>
2024-10-24 15:31:05 +0700

    add image single choice

--- src/SingleChoice.js
+++ src/SingleChoice.js
@@ -10,6 +10,7 @@ import {
   Button,
   Input,
   IconButton,
+  Image,
   ChakraProvider
 } from "@chakra-ui/react";
 import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
@@ -32,9 +33,50 @@ const SingleChoice = ({ answers: initialAnswers }) => {
     setAnswers(answers.filter((_, index) => index !== indexToRemove));
   };
 
+  // Image upload state
+  const [image, setImage] = useState(null);
+
+  // Handle image selection
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setImage(imageUrl);
+    }
+  };
+
   return (
     <ChakraProvider>
       <Box p={5} bg="gray.50" borderRadius="lg" borderWidth="1px" my={4}>
+        {/* Image icon and upload functionality */}
+        <Flex align="center" mb={4}>
+          <IconButton
+            icon={<Image />}
+            aria-label="Upload question image"
+            mr={2}
+            onClick={() => document.getElementById("image-upload").click()}
+          />
+          <input
+            id="image-upload"
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
+          <Editable defaultValue="Type your question here">
+            <EditablePreview />
+            <EditableInput />
+          </Editable>
+        </Flex>
+
+        {/* Display uploaded image */}
+        {image && (
+          <Box mb={4}>
+            <Image src={image} alt="Uploaded question image" maxW="200px" borderRadius="md" />
+          </Box>
+        )}
+
+        {/* Answer options */}
         <RadioGroup>
           {answers.map((answer, index) => (
             <Flex
@@ -65,6 +107,8 @@ const SingleChoice = ({ answers: initialAnswers }) => {
             </Flex>
           ))}
         </RadioGroup>
+
+        {/* Add new answer */}
         <Flex mt={4} align="center">
           <Input
             placeholder="Add new answer"
