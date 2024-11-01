e28c11e5b51421d028e530435a4642f26b4dabf8
HunggSamm <143309756+HunggSamm@users.noreply.github.com>
2024-11-01 10:33:10 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -25,18 +25,16 @@ import Chart from "./LineChart";
 import StudentLearn from "./StudentLearn";
 import CourseCard from "./CourseCard";
 import Learn from "./Learn";
-import Drip from "./Drip";
+import Drip from "./QuoteApp";
 import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const content =
   "<h1><strong>Hung:</strong></h1><p>abc ok </p><p><br></p><p><br></p>";
 
 root.render(
-  <React.StrictMode>
     <ChakraProvider>
       <Drip />
     </ChakraProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
